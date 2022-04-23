0afc05bd1bae31cef6c5fda17d36cce14ea52f07
Mark Ryan Guamos <ryan.guamos@gmail.com>
2022-04-24 03:26:26 +0800

    revert

--- src/components/GameComponent.vue
+++ src/components/GameComponent.vue
@@ -12,8 +12,6 @@
           data-aos-duration="3000"
           src="/src/assets/emblems.png"
           class="w-full"
-          width="900"
-          height="900"
         />
       </div>
       <div
