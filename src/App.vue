2c2917e928ba8c1b22a251577172ed83fa4d032b
Mark Ryan Guamos <ryan.guamos@gmail.com>
2022-04-24 20:48:52 +0800

    adsense

--- src/App.vue
+++ src/App.vue
@@ -86,6 +86,13 @@
         content: 'website',
       },
     ],
+    script: [
+      {
+        async: true,
+        src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3072480337071983',
+        crossorigin: 'anonymous',
+      },
+    ],
   })
 
   const scrollListener = () => {
